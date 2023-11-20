decf433b413a01b7d3b26d77587d0854e01be28f
LinusWeb <linooow@gmail.com>
2023-11-20 13:57:29 -0300

    melhorando as validacoes

--- projeto-final.js
+++ projeto-final.js
@@ -2,90 +2,98 @@ const prompt = require('prompt-sync')();
 
 const listaDeTarefas = []
 
-let opcao;
+let opcaoMenu;
 let tarefa;
+let numeroTarefa;
 
 
 do {
     console.log(`
-    --------------------------
-          Menu principal     
-    --------------------------
+    ---------------------------------
+              Menu principal             
+    ---------------------------------
     1 - Adicionar tarefa.
     2 - Obter apenas uma tarefa.
     3 - Obter todas as tarefas.
     4 - Editar tarefa.
-    5 - Deletar tarefa.
+    5 - Deletar a primeira tarefa.
+    6 - Deletar um tarefa específica.
     0 - Sair.
-    --------------------------
+    ---------------------------------
     `);
 
-    opcao = parseInt(prompt("Digite o número da opção desejada: "));
+    opcaoMenu = parseInt(prompt("Digite o número da opção desejada: "));
 
-    switch (opcao) {
+    switch (opcaoMenu) {
         case 1:
             tarefa = prompt("- Digite uma tarefa: ");
             adicionarTarefa(tarefa);
             break;
+
         case 2:
-            obterTodasAsTarefas();
-            numeroTarefa = parseInt(prompt("- Digite o número da tarefa: "));
-            obterUmaTarefa(numeroTarefa);
-            break;
-        case 3:
-            obterTodasAsTarefas();
-            break;  
-        case 4:
-            obterTodasAsTarefas();
-            numeroTarefa = parseInt(prompt("- Digite o número da tarefa: "));
-            
-            if(isNaN(numeroTarefa)){
-                throw new Error(`> O valor digitado não é válido.`);
+            if (listaDeTarefas.length === 0) {
+                console.log(`> Não existem tarefas cadastradas.`);
+            } else {
+                obterTodasAsTarefas();
+                numeroTarefa = parseInt(prompt("- Digite o número da tarefa: "));
+                obterUmaTarefa(numeroTarefa);
             }
-            
-            novaTarefa = prompt("- Digite a nova tarefa: ");
-            editarTarefa(numeroTarefa, novaTarefa);
+            break;
+
+        case 3:
+            if (listaDeTarefas.length === 0) {
+                console.log(`> Não existem tarefas cadastradas.`);
+            } else {
+                obterTodasAsTarefas();
+            }
             break;  
-        case 5:
-            obterTodasAsTarefas();
-            numeroTarefa = parseInt(prompt("- Digite o número da tarefa: "));
-            deletarTarefa(numeroTarefa);
+        
+        case 4:
+            if (listaDeTarefas.length === 0) {
+                console.log(`> Não existem tarefas cadastradas.`);
+            } else {
+                obterTodasAsTarefas();
+                numeroTarefa = parseInt(prompt("- Digite o número da tarefa: "));
+                editarTarefa(numeroTarefa);            
+            }
             break; 
+
+        case 5:
+            if (listaDeTarefas.length === 0) {
+                console.log(`> Não existem tarefas cadastradas.`);
+            } else {
+                deletarPrimeiraTarefa();
+            }
+            break;
+
+        case 6:
+            if (listaDeTarefas.length === 0) {
+                console.log(`> Não existem tarefas cadastradas.`);
+            } else {
+                obterTodasAsTarefas();
+                numeroTarefa = parseInt(prompt("- Digite o número da tarefa: "));
+                deletarTarefa(numeroTarefa);
+            }
+            break;
+
         case 0:
             console.log(`> Programa finalizado.`);
-            break;                                   
+            break;  
+
         default:
             console.log(`> Opção incorreta! Selecione outra.`);
             break;
     }
 
-} while (opcao != 0);
+} while (opcaoMenu != 0);
+
 
 
 function adicionarTarefa(tarefa) {
     try {
-        if(!tarefa){
-            throw new Error(`> Não é possível cadastrar uma tarefa vazia.`);
-        }
-           
-        if(tarefa.length < 4){
-            throw new Error(`> A tarefa deve ter no mínimo 4 caracteres.`);
-        }
-
-        if(tarefa.length > 20){
-            throw new Error(`> A tarefa deve ter no máximo 20 caracteres.`);
-        }
-
-        const tarefaExiste = listaDeTarefas.includes(tarefa);
-
-        if(tarefaExiste){
-            throw new Error(`> Esta tarefa já está cadastrada.`);
-        }
-        
+        verificarTarefa(tarefa);
         listaDeTarefas.push(tarefa);
-
-        console.log(`> Tarefa adicionada com sucesso.`);
-        
+        console.log(`> Tarefa adicionada com sucesso.`);      
         obterTodasAsTarefas();
 
     } catch (e) {
@@ -96,18 +104,11 @@ function adicionarTarefa(tarefa) {
 }
 
 
+
 function obterUmaTarefa(numeroTarefa) {
     try {
-        if(isNaN(numeroTarefa)){
-            throw new Error(`> O valor digitado não é válido.`);
-        }
-    
-        if(numeroTarefa <= 0 || numeroTarefa > listaDeTarefas.length) {
-            throw new Error(`> Tarefa não localizada.`);
-        }
-
-        const tarefa = listaDeTarefas[numeroTarefa - 1]
-        
+        verificarSeValorEValidoETarefaExiste(numeroTarefa);
+        tarefa = listaDeTarefas[numeroTarefa - 1]
         console.log(tarefa);
 
     } catch (e) {
@@ -118,52 +119,57 @@ function obterUmaTarefa(numeroTarefa) {
 }
 
 
+
 function obterTodasAsTarefas() {
-    try {
-        if(listaDeTarefas.length === 0) {
-            throw new Error(`> Não existem Tarefas cadastradas.`);
-        }
+    console.log(``);
+    console.log(`- Tarefas cadastradas:`);
 
-        console.log(``);
-        console.log(`- Tarefas cadastradas:`);
-
-        for (let i = 0; i < listaDeTarefas.length; i++) {
-            console.log(`${i + 1} - ${listaDeTarefas[i]}`);
-        }       
-    } catch (e) {
-        console.error({"name": e.name,
-                       "mensagem": e.message,
-                       "data": Date()})           
+    for (let i = 0; i < listaDeTarefas.length; i++) {
+        console.log(`${i + 1} - ${listaDeTarefas[i]}`);
     }
 }
 
 
-function editarTarefa(numeroTarefa, novaTarefa) {  
-    try {
-        if (numeroTarefa < 1 || numeroTarefa > listaDeTarefas.length) {
-            throw new Error(`> Tarefa não localizada.`);
-        }
 
-        listaDeTarefas.splice(numeroTarefa - 1, 1, novaTarefa);
+function editarTarefa(numeroTarefa) {  
+    try {   
+        verificarSeValorEValidoETarefaExiste(numeroTarefa);
+        tarefa = prompt("- Digite a nova tarefa: ");
+        verificarTarefa(tarefa);
+        listaDeTarefas.splice(numeroTarefa - 1, 1, tarefa);
         console.log(`> Tarefa modificada com sucesso.`);
         obterTodasAsTarefas();
+
     } catch (e) {
         console.error({"name": e.name,
                        "mensagem": e.message,
                        "data": Date()})           
     }
 }
+
+
+
+function deletarPrimeiraTarefa() {
+    try {
+        listaDeTarefas.shift();
+        console.log(`> Tarefa removida com sucesso.`);
+        obterTodasAsTarefas();
+        
+    } catch (e) {
+        console.error({"name": e.name,
+                       "mensagem": e.message,
+                       "data": Date()})           
+    }    
+}
+
 
 
 function deletarTarefa(numeroTarefa) {
     try {
-        if (numeroTarefa < 1 || numeroTarefa > listaDeTarefas.length) {
-            throw new Error(`> Tarefa não localizada.`);
-        }
-
+        verificarSeValorEValidoETarefaExiste(numeroTarefa);
         listaDeTarefas.splice(numeroTarefa - 1, 1);
         console.log(`> Tarefa removida com sucesso.`);
-        
+    
         if(listaDeTarefas.length === 0) {
             console.log(`> Não existem tarefas cadastradas.`);
         } else {
@@ -175,4 +181,37 @@ function deletarTarefa(numeroTarefa) {
                        "mensagem": e.message,
                        "data": Date()})           
     }    
+}
+
+
+
+function verificarSeValorEValidoETarefaExiste(numeroTarefa) {
+    if(isNaN(numeroTarefa)){
+        throw new Error(`> O valor digitado não é válido.`);
+    }
+
+    if(numeroTarefa < 1 || numeroTarefa > listaDeTarefas.length) {
+        throw new Error(`> Tarefa não localizada.`);
+    }         
+}
+
+
+function verificarTarefa(tarefa) {
+    if(!tarefa){
+        throw new Error(`> Não é possível cadastrar uma tarefa vazia.`);
+    }
+       
+    if(tarefa.length < 4){
+        throw new Error(`> A tarefa deve ter no mínimo 4 caracteres.`);
+    }
+
+    if(tarefa.length > 20){
+        throw new Error(`> A tarefa deve ter no máximo 20 caracteres.`);
+    }
+
+    const tarefaExiste = listaDeTarefas.includes(tarefa);
+
+    if(tarefaExiste){
+        throw new Error(`> Esta tarefa já está cadastrada.`);
+    }
 }
