547a5a5561f44d8c7d73b457af41aca60d987a0c
Igor-Benez <103526457+Igor-Benez@users.noreply.github.com>
2023-11-20 22:01:04 -0300

    Refaturando a funcao obterTodasAsTarefas

--- projeto-final.js
+++ projeto-final.js
@@ -6,6 +6,129 @@ let opcaoMenu;
 let tarefa;
 let numeroTarefa;
 
+const obterTodasAsTarefas = () => {
+    console.log(``);
+    console.log(`- Tarefas cadastradas:`);
+
+    for (let i = 0; i < listaDeTarefas.length; i++) {
+        console.log(`${i + 1} - ${listaDeTarefas[i]}`);
+    }
+};
+
+function adicionarTarefa(tarefa) {
+    try {
+        verificarTarefa(tarefa);
+        listaDeTarefas.push(tarefa);
+        console.log(`> Tarefa adicionada com sucesso.`);      
+        obterTodasAsTarefas();
+
+    } catch (e) {
+        console.error({"name": e.name,
+                       "mensagem": e.message,
+                       "data": Date()})
+    }
+}
+
+function obterUmaTarefa(numeroTarefa) {
+    try {
+        verificarSeValorEValidoETarefaExiste(numeroTarefa);
+        tarefa = listaDeTarefas[numeroTarefa - 1]
+        console.log(tarefa);
+
+    } catch (e) {
+        console.error({"name": e.name,
+                       "mensagem": e.message,
+                       "data": Date()})        
+    }
+}
+
+function editarTarefa(numeroTarefa) {  
+    try {   
+        verificarSeValorEValidoETarefaExiste(numeroTarefa);
+        tarefa = prompt("- Digite a nova tarefa: ");
+        verificarTarefa(tarefa);
+        listaDeTarefas.splice(numeroTarefa - 1, 1, tarefa);
+        console.log(`> Tarefa modificada com sucesso.`);
+        obterTodasAsTarefas();
+
+    } catch (e) {
+        console.error({"name": e.name,
+                       "mensagem": e.message,
+                       "data": Date()})           
+    }
+}
+
+function deletarPrimeiraTarefa() {
+    try {
+        listaDeTarefas.shift();
+        console.log(`> Tarefa removida com sucesso.`);
+        obterTodasAsTarefas();
+        
+    } catch (e) {
+        console.error({"name": e.name,
+                       "mensagem": e.message,
+                       "data": Date()})           
+    }    
+}
+
+function deletarTarefa(numeroTarefa) {
+    try {
+        verificarSeValorEValidoETarefaExiste(numeroTarefa);
+        listaDeTarefas.splice(numeroTarefa - 1, 1);
+        console.log(`> Tarefa removida com sucesso.`);
+    
+        if(listaDeTarefas.length === 0) {
+            console.log(`> Não existem tarefas cadastradas.`);
+        } else {
+            obterTodasAsTarefas();
+        }
+        
+    } catch (e) {
+        console.error({"name": e.name,
+                       "mensagem": e.message,
+                       "data": Date()})           
+    }    
+}
+
+
+
+function verificarSeValorEValidoETarefaExiste(numeroTarefa) {
+    if(isNaN(numeroTarefa)){
+        throw new Error(`> O valor digitado não é válido.`);
+    }
+
+    if(numeroTarefa < 1 || numeroTarefa > listaDeTarefas.length) {
+        throw new Error(`> Tarefa não localizada.`);
+    }         
+}
+
+
+function verificarTarefa(tarefa) {
+    if(!tarefa){
+        throw new Error(`> Não é possível cadastrar uma tarefa vazia.`);
+    }
+       
+    if(tarefa.length < 4){
+        throw new Error(`> A tarefa deve ter no mínimo 4 caracteres.`);
+    }
+
+    if(tarefa.length > 20){
+        throw new Error(`> A tarefa deve ter no máximo 20 caracteres.`);
+    }
+
+    const tarefaExiste = listaDeTarefas.includes(tarefa);
+
+    if(tarefaExiste){
+        throw new Error(`> Esta tarefa já está cadastrada.`);
+    }
+}
+
+function removerTodasTarefas(){
+    while (listaDeTarefas.length > 0) {
+        listaDeTarefas.pop();
+    }
+    console.log(`> Todas as tarefas foram deletadas.`)
+}
 
 do {
     console.log(`
@@ -97,136 +220,7 @@ do {
 
 
 
-function adicionarTarefa(tarefa) {
-    try {
-        verificarTarefa(tarefa);
-        listaDeTarefas.push(tarefa);
-        console.log(`> Tarefa adicionada com sucesso.`);      
-        obterTodasAsTarefas();
-
-    } catch (e) {
-        console.error({"name": e.name,
-                       "mensagem": e.message,
-                       "data": Date()})
-    }
-}
 
 
 
-function obterUmaTarefa(numeroTarefa) {
-    try {
-        verificarSeValorEValidoETarefaExiste(numeroTarefa);
-        tarefa = listaDeTarefas[numeroTarefa - 1]
-        console.log(tarefa);
 
-    } catch (e) {
-        console.error({"name": e.name,
-                       "mensagem": e.message,
-                       "data": Date()})        
-    }
-}
-
-
-
-function obterTodasAsTarefas() {
-    console.log(``);
-    console.log(`- Tarefas cadastradas:`);
-
-    for (let i = 0; i < listaDeTarefas.length; i++) {
-        console.log(`${i + 1} - ${listaDeTarefas[i]}`);
-    }
-}
-
-
-
-function editarTarefa(numeroTarefa) {  
-    try {   
-        verificarSeValorEValidoETarefaExiste(numeroTarefa);
-        tarefa = prompt("- Digite a nova tarefa: ");
-        verificarTarefa(tarefa);
-        listaDeTarefas.splice(numeroTarefa - 1, 1, tarefa);
-        console.log(`> Tarefa modificada com sucesso.`);
-        obterTodasAsTarefas();
-
-    } catch (e) {
-        console.error({"name": e.name,
-                       "mensagem": e.message,
-                       "data": Date()})           
-    }
-}
-
-
-
-function deletarPrimeiraTarefa() {
-    try {
-        listaDeTarefas.shift();
-        console.log(`> Tarefa removida com sucesso.`);
-        obterTodasAsTarefas();
-        
-    } catch (e) {
-        console.error({"name": e.name,
-                       "mensagem": e.message,
-                       "data": Date()})           
-    }    
-}
-
-
-
-function deletarTarefa(numeroTarefa) {
-    try {
-        verificarSeValorEValidoETarefaExiste(numeroTarefa);
-        listaDeTarefas.splice(numeroTarefa - 1, 1);
-        console.log(`> Tarefa removida com sucesso.`);
-    
-        if(listaDeTarefas.length === 0) {
-            console.log(`> Não existem tarefas cadastradas.`);
-        } else {
-            obterTodasAsTarefas();
-        }
-        
-    } catch (e) {
-        console.error({"name": e.name,
-                       "mensagem": e.message,
-                       "data": Date()})           
-    }    
-}
-
-
-
-function verificarSeValorEValidoETarefaExiste(numeroTarefa) {
-    if(isNaN(numeroTarefa)){
-        throw new Error(`> O valor digitado não é válido.`);
-    }
-
-    if(numeroTarefa < 1 || numeroTarefa > listaDeTarefas.length) {
-        throw new Error(`> Tarefa não localizada.`);
-    }         
-}
-
-
-function verificarTarefa(tarefa) {
-    if(!tarefa){
-        throw new Error(`> Não é possível cadastrar uma tarefa vazia.`);
-    }
-       
-    if(tarefa.length < 4){
-        throw new Error(`> A tarefa deve ter no mínimo 4 caracteres.`);
-    }
-
-    if(tarefa.length > 20){
-        throw new Error(`> A tarefa deve ter no máximo 20 caracteres.`);
-    }
-
-    const tarefaExiste = listaDeTarefas.includes(tarefa);
-
-    if(tarefaExiste){
-        throw new Error(`> Esta tarefa já está cadastrada.`);
-    }
-}
-
-function removerTodasTarefas(){
-    while (listaDeTarefas.length > 0) {
-        listaDeTarefas.pop();
-    }
-    console.log(`> Todas as tarefas foram deletadas.`)
-}
